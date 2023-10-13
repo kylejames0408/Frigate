f599e773c8860d471e13d69131b8f56455c983b8
ZaffyPuck <56975883+ZaffyPuck@users.noreply.github.com>
2023-10-13 19:42:03 -0400

    Moved BuildingData scriptable object to separate file

--- Libertatia/Assets/Scripts/ScriptableObjects/PlayerData.cs
+++ Libertatia/Assets/Scripts/ScriptableObjects/PlayerData.cs
@@ -109,17 +109,3 @@ public class PlayerDataManager
         data.gameTimer = gameTimer;
     }
 }
-
-// Components that all buildings share
-[CreateAssetMenu(fileName = "BuildingComponents", menuName = "Libertatia/BuildingComponents", order = 2)]
-public class BuildingComponents : ScriptableObject
-{
-    // Materials
-    public Material placingMaterial;
-    public Material collisionMaterial;
-    public Material needsAssignmentMaterial;
-    public Material buildingMaterial;
-    // Triggerables
-    public ParticleSystem buildParticle;
-    public ParticleSystem finishParticle;
-}
