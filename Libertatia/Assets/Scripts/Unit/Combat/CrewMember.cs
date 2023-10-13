1655195161a109582ce7226192b8522d3b441d60
Winson Weng (RIT Student) <wqw7732@rit.edu>
2023-10-13 12:51:50 -0400

    Updated crew members

--- Libertatia/Assets/Scripts/Unit/Combat/CrewMember.cs
+++ Libertatia/Assets/Scripts/Unit/Combat/CrewMember.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CrewMember : Character
 {
     public List<Character> enemies;
+    public List<GameObject> enemyGameobjects;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,14 @@ public class CrewMember : Character
         attackRange = 2;
         attackRate = 2;
         damage = 25;
+
+        enemyGameobjects.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+
+        for (int i = 0; i < enemyGameobjects.Count; i++)
+        {
+            Character crewMember = enemyGameobjects[i].GetComponent<Character>();
+            enemies.Add(crewMember);
+        }
     }
 
     // Update is called once per frame
