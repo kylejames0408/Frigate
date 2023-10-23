39bcd7440ddb7860e0c09f56633b2917b1c7ddc5
Winson Weng (RIT Student) <wqw7732@rit.edu>
2023-10-23 12:39:05 -0400

    Started on combat ui

---
+++ Libertatia/Assets/Scripts/UI/CombatUI/CombatUI.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CombatUI : MonoBehaviour
+{
+    public TextMeshProUGUI tmpCrewmateAmt;
+    public TextMeshProUGUI tmpCrewmateCapacity;
+    public TextMeshProUGUI tmpFoodAmt;
+    public TextMeshProUGUI tmpFoodConsumption;
+    public TextMeshProUGUI tmpDubloonAmt;
+    public TextMeshProUGUI tmpWoodAmt;
+    public TextMeshProUGUI tmpStoneAmt;
+
+    public void Init()
+    {
+        PlayerData data = GameManager.Data;
+        tmpCrewmateAmt.text = data.crewmates.Count.ToString();
+        tmpCrewmateCapacity.text = data.outpostCrewCapacity.ToString();
+        tmpFoodAmt.text = data.resources.food.ToString();
+        tmpFoodConsumption.text = data.resources.foodPerAP.ToString();
+        tmpDubloonAmt.text = data.resources.doubloons.ToString();
+        tmpWoodAmt.text = data.resources.wood.ToString();
+    }
+
+    public void UpdateCrewAmountUI(int crewAmt)
+    {
+        tmpCrewmateAmt.text = crewAmt.ToString();
+    }
+    public void UpdateCrewCapacityUI(int crewCapacity)
+    {
+        tmpCrewmateCapacity.text = crewCapacity.ToString();
+    }
+    public void UpdateFoodAmountUI(int foodAmt)
+    {
+        tmpFoodAmt.text = foodAmt.ToString();
+    }
+    public void UpdateFoodConsumptionUI(int consumption)
+    {
+        tmpFoodConsumption.text = consumption.ToString();
+    }
+    public void UpdateDubloonUI(int dubloonAmt)
+    {
+        tmpDubloonAmt.text = dubloonAmt.ToString();
+    }
+    public void UpdateWoodUI(int woodAmt)
+    {
+        tmpWoodAmt.text = woodAmt.ToString();
+    }
+}
