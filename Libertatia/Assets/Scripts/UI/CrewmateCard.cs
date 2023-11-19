9324a109b36318b4e9d7c81f3dbaa0cef05e053b
ZaffyPuck <56975883+ZaffyPuck@users.noreply.github.com>
2023-11-18 19:21:31 -0500

    Revert "Merge pull request #38 from kylejames0408/dev-merge-test-2"
    This reverts commit da14f08292b7602f0defbb80996e5b0bb72a5c76, reversing
    changes made to cceaa0ae7acecdc42d86d1a47e3683c30d7279d4.

--- Libertatia/Assets/Scripts/UI/CrewmateCard.cs
+++ Libertatia/Assets/Scripts/UI/CrewmateCard.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CrewmateCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
